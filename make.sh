a0b72f0a89bd4d3025c8bce02eb9a2a296a8e9f7
GenTum-coder <tga001@inbox.ru>
2022-11-19 19:04:15 +0300

    Update
    Signed-off-by: GenTum-coder <tga001@inbox.ru>

--- make.sh
+++ make.sh
@@ -8,11 +8,7 @@ else
     mkdir build
 fi
 
-#riscv-nuclei-elf-as $1.S -o $1.o
-#riscv-nuclei-elf-ld $1.o -o $1.elf
 echo Compiling ...
-#riscv-nuclei-elf-gcc -march=rv32imac -mabi=ilp32 -mcmodel=medany -nostdlib $1.S GD32VF103x8.lds -o $1.elf
-#riscv-nuclei-elf-gcc -march=rv32imac -mabi=ilp32 -mcmodel=medany -nostdlib $1.S -o $1.elf
 riscv64-unknown-elf-gcc -march=rv32imac -mabi=ilp32 -mcmodel=medany -nostdlib -T eeprom.ld $1.S -o build/$1.elf
 echo ELF to LST
 riscv-nuclei-elf-objdump -S build/$1.elf > build/$1.lst
@@ -21,8 +17,7 @@ riscv-nuclei-elf-objcopy -O binary build/$1.elf build/$1.bin
 echo ELF to LSS
 riscv-nuclei-elf-objdump -D -S build/$1.elf > build/$1.lss
 echo Flashing ...
-#stm32flash /dev/ttyUSB0 -w main.bin
-#stm32flash -v -g 0x08000000 /dev/ttyUSB0 -w $1.bin
-stm32flash -v -g 0x0 /dev/ttyUSB0 -w build/$1.bin
-echo Start ...
+#stm32flash -v -g 0x0 /dev/ttyUSB0 -w build/$1.bin
+#echo Start ...
+echo End ...
 
